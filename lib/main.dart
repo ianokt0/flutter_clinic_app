01f941b04892fcc2ef09a23f026b0681a63438cc
ianokt0 <ian.gutho16@gmail.com>
2024-04-04 06:06:08 +0700

    initiallize projcet

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:clinic_app/core/constants/colors.dart';
+import 'package:clinic_app/precentation/auth/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clinic App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.quicksand(
+            color: AppColors.primary,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          iconTheme: const IconThemeData(
+            color: AppColors.primary,
+          ),
+        ),
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
